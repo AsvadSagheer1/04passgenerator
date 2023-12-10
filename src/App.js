d063524c1642b9bf68c37c91c413030a131faec2
AsvadSagheer1 <92682536+AsvadSagheer1@users.noreply.github.com>
2023-12-10 14:02:51 +0500

    Created functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useCallback, useRef } from 'react'
 
 function App() {
+  const [length, setLength] = useState(8)
+  const [pass, setPass] = useState("")
+  const [charAllowed, setcharAllowed] = useState(false)
+  const [numberAllowed, setnumberAllowed] = useState(false)
+
+  const passGenerator = useCallback(
+    () => {
+      let pass = ""
+      let str =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+      if (charAllowed) str += "~`!@#$%^&*()-_=+|]}[{';:/?.>,<"
+      if (numberAllowed) str += "0123456789"
+
+      for (let i = 0; i < length; i++) {
+        let char = Math.floor(Math.random() * str.length)
+        pass += str.charAt(char)
+
+      }
+      setPass(pass)
+
+    },
+    [length, setPass, charAllowed, numberAllowed],
+  )
+
+  useEffect(() => {
+    passGenerator()
+  },
+    [length, numberAllowed, charAllowed, passGenerator])
+
+  const passRef = useRef(null)
+  const copyPasswordToClipboard = useCallback(() => { 
+    passRef.current?.select()
+    passRef.current?.setSelectionRange(0, 100)
+    window.navigator.clipboard.writeText(pass) 
+  
+},
+    [pass])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-center">Password Generator</h1>
+      <input
+        type="text"
+        value={pass}
+        placeholder='Password'
+        readOnly
+        ref={passRef}
+      />
+      <button
+        className='btn btn-primary'
+        onClick={copyPasswordToClipboard}>
+        Copy</button>
+      <input
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+        onChange={(e) => setLength(e.target.value)}
+      />
+      <label>Length: {length}</label>
+      <label htmlFor='numInput'>Numbers {numberAllowed}</label>
+      <input
+        type="checkbox"
+        defaultChecked={numberAllowed}
+        id='numInput'
+        onChange={() =>
+          setnumberAllowed((prev) => !prev)
+        }
+      />
+      <label htmlFor='charInput'>Characters {charAllowed}</label>
+      <input
+        type="checkbox"
+        defaultChecked={charAllowed}
+        id='charInput'
+        onChange={() =>
+          setcharAllowed((prev) => !prev)
+        }
+      />
+    </>
   );
 }
 
